d1162d8744a295bf30c4894d2d8e8b4889d2acc1
github username <github email address>
2020-06-14 03:55:54 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,23 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: bird, only: [:id, :name, :species]
+    elsif !params[:id].to_i
+      render json: { message: "Please use numerical ids on this route"}
+    else
+      render json: { message: "Bird not found" }
+    end
+
+    private
+
+    def bird_params
+      params.require(:birds).permit(:name, :species)
+    end
   end
 end
